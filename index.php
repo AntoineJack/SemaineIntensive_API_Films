48b732e29bfbe8fd7072a1e983b120bfbed822e2
AntoineJack <jacotyantoine@gmail.com>
2016-04-11 12:17:15 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+	    <title>Première pierre de notre site "Trouve un super film !"</title>
+	    <link rel="stylesheet" href="src/css/reset.css">
+	    <link rel="stylesheet" href="src/css/style.css">
+	</head>
+
+	<body>
+	    
+	<?= 'Hello world' ?>
+
+
+	    <script src="src/js/libs/jquery-2.2.0.min.js"></script>
+	    <script src="src/js/app/script.js"></script>
+	</body>
+
+</html>            
